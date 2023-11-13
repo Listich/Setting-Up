3762afd98870ce18666b21b5244dfb20cc99e8e6
serenachristy <serena.kifoula@epitech.eu>
2023-11-13 10:45:19 +0100

    ADD: files solver

---
+++ setting_up.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2023
+** bsq
+** File description:
+** bsq.c
+*/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "include/setting_up.h"
+
+
+void display_unit(map_t *map, square_t *square, int i, int j)
+{
+    if (is_inside(square, i , j)){
+        write(1, "x", sizeof(char));
+    } else {
+        write(1, &map->lines[i][j], sizeof(char));
+    }
+}
+
+void display_lines(map_t *map, square_t *square)
+{
+    for (int i = 0; i < map->height ; i++) {
+        for (int j = 0; j < map->width; j++) {
+            display_unit(map, square, i, j);
+        }
+        write(1, "\n", sizeof(char));
+    }
+}
+
+int main(int ac, char **av)
+{
+    int fd = open(av[1], O_RDONLY);
+    map_t *map = init_map(fd);
+    square_t *square;
+
+    square = find_bsq(map);
+    display_lines(map, square);
+    printf("%d\n", square->length);
+    printf(" %d %d\n", square->pos->x, square->pos->y);
+    free(map);
+}
---
+++ solver.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2023
+** bsq
+** File description:
+** solver.c
+*/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "include/setting_up.h"
+
+int is_valid_sq(square_t *square)
+{
+    for (int i = square->pos->x; i < (square->pos->x + square->length); i++) {
+        for (int j = square->pos->y; j <
+        (square->pos->y + square->length); j++) {
+            if (square->pos->lines[i][j] != '.') {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int biggest_valid_sq(map_t *map, int x, int y)
+{
+    position_t pos = {
+        .lines = map->lines, .x = x, .y = y,
+    };
+    square_t square = {
+        .pos = &pos, .length = 0,
+    };
+    while (is_valid_sq(&square) &&
+    is_inbound(map, x + square.length, y + square.length)) {
+        square.length++;
+    }
+    return square.length -1;
+}
+
+void update_square(square_t *square, int length, int x, int y)
+{
+    if (length > square->length) {
+        square->length = length;
+        square->pos->x = x;
+        square->pos->y = y;
+    }
+}
+
+square_t *find_bsq(map_t *map)
+{
+    position_t *pos = malloc(sizeof(position_t));
+    square_t *square;
+    int current;
+
+    pos->lines = map->lines;
+    pos->x = -1;
+    pos->y = -1;
+    square = malloc(sizeof(square_t));
+    square->pos = pos;
+    square->length = 0;
+    for (int i = 0; i < map->height; i++) {
+        for (int j = 0; j < map->width; j++) {
+            current = biggest_valid_sq(map, i, j);
+            update_square(square, current, i, j);
+        }
+    }
+    return square;
+}
